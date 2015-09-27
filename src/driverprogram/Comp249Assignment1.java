8eedf11f429cd1fcd7057dba317437a7c9fb110c
Louis-Olivier Guérin <guerin.louis@gmail.com>
2015-09-27 16:35:42 -0400

    Remove unneeded objects

--- src/driverprogram/Comp249Assignment1.java
+++ src/driverprogram/Comp249Assignment1.java
@@ -15,32 +15,15 @@ import booktypes.*;
 import dailypublication.*;
 import monthlypublication.*;
 import paperpublication.*;
-import java.util.*;
 
 public class Comp249Assignment1 {
 
     public static void main(String[] args) {
         //Welcome message
-        System.out.println("Welcome to the COMP 249 Assignemt 1 Part 1 program. My name is Louis-Olivier Guérin. Let's begin!");
-        
-        //Program starts here
-        double price;
-        int numberOfPages;
-        long isbn;
-        int issueYear;
-        int issueNumber;
-        int editionNumber;
-        int minimumAge;
-        String authorName;
-        String title;
-        String specialityField;
-        
-        Scanner keyboard = new Scanner(System.in);
-        
-        
+        System.out.println("Welcome to the COMP 249 Assignemt 1 Part 1 program. My name is Louis-Olivier Guérin. Let's get started!");
+    
         //Creating PaperPublication instances
         PaperPublication myPaperPublication = new PaperPublication();
-        
         PaperPublication myParameterizedPaperPublication  = new PaperPublication(14.99, 254);
         PaperPublication myParameterizedPaperPublication2  = new PaperPublication(14.99, 254);
         System.out.println(myPaperPublication);
@@ -51,10 +34,13 @@ public class Comp249Assignment1 {
         
         //Creating Book instances
         Book myBook = new Book();
-        Book myParameterizedBook = new Book(2009, 7342867754835L, "Bob Chanty", 34.99, 784, "My life: Revisited");
+        myBook.setAuthorName("John Davis");
+        myBook.setIsbn(385939696);
+        myBook.setIssueYear(2001);
+        myBook.setNumberOfPages(345);
+        myBook.setPrice(45.88);
+        myBook.setTitle("Flying");
         System.out.println(myBook);
-        System.out.println(myParameterizedBook);
-        System.out.println(myBook.equals(myParameterizedBook));
         System.out.println(myBook.equals(myBook));
         
         
@@ -82,9 +68,10 @@ public class Comp249Assignment1 {
         System.out.println(myChildrenBook);
         System.out.println(myParameterizedChildrenBook);
         
+        //Create an array and fill it with various objects
         PaperPublication[] myPaperPublicationArray = 
                                                     {myParameterizedPaperPublication,
-                                                     myParameterizedBook,
+                                                     myBook,
                                                      myJournal,
                                                      myParameterizedJournal,
                                                      myMagazine,
@@ -97,6 +84,7 @@ public class Comp249Assignment1 {
         double smallest = myPaperPublicationArray[0].getPrice();
         double largest = myPaperPublicationArray[0].getPrice();
         
+        //Trace the PaperPublication array to find the cheapest pricest
         for (PaperPublication object : myPaperPublicationArray) {
             if (object.getPrice() > largest) {
                 largest = object.getPrice();
@@ -104,6 +92,7 @@ public class Comp249Assignment1 {
                 smallest = object.getPrice();
             }  
         }
+        //Print cheapest price
         System.out.println("Cheapest price in the array is: " + smallest + "$.");
 
     };

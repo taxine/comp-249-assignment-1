e2d713757d87e935bffc8b4dbefc50eaf63f2ffc
taxine <chromecanyons@gmail.com>
2015-10-01 22:48:44 -0400

    the assignment asks to print all the info for the cheapest object so i rewrote that block of code in the driver

--- Desktop/comp-249-assignment-1-master/src/driverprogram/Comp249Assignment1.java
+++ Desktop/comp-249-assignment-1-master/src/driverprogram/Comp249Assignment1.java
@@ -86,17 +86,18 @@ public class Comp249Assignment1 {
         
         PaperPublication smallest = myPaperPublicationArray[0];
         PaperPublication largest = myPaperPublicationArray[0];
-        
+        int smallestindex = 0;
         //Trace the PaperPublication array to find the cheapest price
         for (int i=0;i<myPaperPublicationArray.length;i++) {
             if (myPaperPublicationArray[i].getPrice() > largest.getPrice()) {
                 largest = myPaperPublicationArray[i];
             } else if (myPaperPublicationArray[i].getPrice() < smallest.getPrice()) {
                 smallest = myPaperPublicationArray[i];
+                smallestindex = i;
             }  
         }
         //Print cheapest price
-        System.out.printf("Cheapest item in the array is:" +smallest );
+        System.out.printf("Cheapest item in the array is:" +smallest+ ". It is found at index " +smallestindex);
 
     };
     
